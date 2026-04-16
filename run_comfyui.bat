294bf4964f247058d3b3fbb5080e506a7605c25d
Neil Adam <neiladam91@yahoo.de>
2026-04-16 16:10:53 +0200

    added ffmpeg

--- run_comfyui.bat
+++ run_comfyui.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d %~dp0
 call venv\Scripts\activate.bat
-python main.py --enable-manager --listen 0.0.0.0 --port 8000 %*
+python main.py --enable-manager --listen 0.0.0.0 --port 8000 --use-sage-attention %*
 pause
